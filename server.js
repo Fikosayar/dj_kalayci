bfe0e522b5bfaae6690bd4252d6501d2f85b170b
Furkan Kalaycı <fikiruret@gmail.com>
2026-06-04 14:56:16 +0300

    fix: Direct ALSA hw:0,0 bypass and add ALSA_CARD=0 env

--- server.js
+++ server.js
@@ -214,8 +214,8 @@ app.post('/api/music/play-server', (req, res) => {
 
     // mpg123 ile sunucu üzerinden çalma (Sade ve güçlü mp3 çözücü)
     // dmix (yazılımsal karıştırıcı) hatalarını (unable to open slave) aşmak için 
-    // doğrudan donanıma (plughw:0,0) bağlanıyoruz.
-    currentServerProcess = exec(`mpg123 -o alsa -a plughw:0,0 "${filePath}"`, (error, stdout, stderr) => {
+    // doğrudan donanıma (hw:0,0) bağlanıyoruz.
+    currentServerProcess = exec(`mpg123 -o alsa -a hw:0,0 "${filePath}"`, (error, stdout, stderr) => {
         if (error) {
             console.error('Sunucuda çalma hatası (mpg123):', error.message);
         }
